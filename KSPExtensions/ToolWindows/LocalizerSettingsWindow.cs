ed3d5ef0374c66daea8df091028668d94e6d68a4
TriggerAu <dtregoning@outlook.com.au>
2017-06-07 22:50:51 +1000

    Updated Menus and Icons

--- KSPExtensions/ToolWindows/LocalizerSettingsWindow.cs
+++ KSPExtensions/ToolWindows/LocalizerSettingsWindow.cs
@@ -29,7 +29,7 @@ namespace KSPExtensions.ToolWindows
         /// </summary>
         public LocalizerSettingsWindow() : base(null)
         {
-            this.Caption = "LocalizerSettingsWindow";
+            this.Caption = "KSP Localizer Refactor Settings";
 
             // This is the user control hosted by the tool window; Note that, even if this class implements IDisposable,
             // we are not calling Dispose on this object. This is because ToolWindowPane calls Dispose on
